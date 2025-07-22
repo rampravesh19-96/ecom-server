d3dcfb8dde678f7a94a8fff5efd61eb74a4704aa
ram pravesh <rampraveshsingh1996@gmail.com>
2025-07-22 23:34:29 +0530

    changes

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 8080;
 
 app.use(
   cors({
-    origin: ["http://localhost:5173","https://ecom-client-phi.vercel.app"],
+    origin: "https://ecom-client-phi.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
