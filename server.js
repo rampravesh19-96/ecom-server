d4013e0802b991f9bbb12d28d3944c27e31a6bf5
ram pravesh <rampraveshsingh1996@gmail.com>
2025-07-22 22:11:57 +0530

    added /  route

--- server.js
+++ server.js
@@ -43,6 +43,9 @@ app.use(
 
 app.use(cookieParser());
 app.use(express.json());
+app.get("/", (req, res) => {
+  res.send("✅ Server is live!");
+});
 app.use("/api/auth", authRouter);
 app.use("/api/admin/products", adminProductsRouter);
 app.use("/api/admin/orders", adminOrderRouter);
